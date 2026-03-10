3c2e11845cc1d8530fadac2afa1d5ced2296836b
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-03-10 15:53:44 +0430

    Add rate limiting middleware to email generation route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\TempEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TempEmailController::class, 'index'])->name('home');
-Route::post('/generate', [TempEmailController::class, 'generate'])->name('email.generate');
+Route::post('/generate', [TempEmailController::class, 'generate'])
+    ->middleware('throttle:generate-email')
+    ->name('email.generate');
 Route::get('/inbox', [TempEmailController::class, 'inbox'])->name('inbox');
 Route::get('/inbox/{id}', [TempEmailController::class, 'showMessage'])->name('message.show');
 Route::get('/api/poll', [TempEmailController::class, 'poll'])->name('api.poll');
