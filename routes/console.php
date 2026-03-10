c11925dbdb094201f9ee8c5476977de6f5baa48c
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-03-10 16:03:39 +0430

    updated

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Email;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 
 // Fetch new emails from IMAP every minute
@@ -8,7 +9,22 @@ Schedule::command('emails:fetch')->everyMinute()
     ->withoutOverlapping()
     ->name('fetch-emails');
 
-// Clean up expired emails and their messages every hour
+// Clean up expired emails in batches every hour
 Schedule::call(function () {
-    Email::where('expires_at', '<', now())->delete();
+    $batchSize = config('tempmail.cleanup_batch_size');
+    $totalDeleted = 0;
+
+    do {
+        $deleted = Email::where('expires_at', '<', now())
+            ->limit($batchSize)
+            ->delete();
+
+        $totalDeleted += $deleted;
+    } while ($deleted === $batchSize);
+
+    if ($totalDeleted > 0) {
+        Log::channel('tempmail')->info('Cleanup: deleted expired emails', [
+            'count' => $totalDeleted,
+        ]);
+    }
 })->hourly()->name('cleanup-expired-emails');
